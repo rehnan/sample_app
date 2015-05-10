bd120b529886d0c31c3183500eff8489c1ba7ef8
Rehnan Carolino <rehnancarolino@live.com>
2015-05-10 20:44:16 -0300

    bugfix remove line remember_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
       redirect_to user
     else
-      # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
