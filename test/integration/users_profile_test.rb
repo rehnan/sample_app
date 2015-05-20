216bc62c848b451399878e72eb2e68e556648cbd
Rehnan Carolino <rehnancarolino@live.com>
2015-05-20 13:53:50 -0300

    Exercises: Chapter 10, done!

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -8,8 +8,9 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   end
 
   test "profile display" do
+    log_in_as(@user)
     get user_path(@user)
-    assert_template 'users/show'
+    assert_template 'users/show' 
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
