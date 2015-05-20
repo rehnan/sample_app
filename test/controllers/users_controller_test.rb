ce53ee621a72dc788f8c2aaeaeeff28579c81903
Rehnan Carolino <rehnancarolino@live.com>
2015-05-20 15:28:44 -0300

    Exercises: Chapter 9, done!

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -37,6 +37,15 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { password:              @other_user.password,
+                                            password_confirmation: @other_user.password,
+                                            admin: true }
+    assert_not @other_user.reload.admin?
+  end
+
   test "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,17 +1,32 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "users non-logged-in layout links" do
   	get root_path
   	assert_template 'static_pages/home'
   	assert_select "a[href=?]", root_path, count: 2
   	assert_select "a[href=?]", help_path
   	assert_select "a[href=?]", about_path
   	assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path, count: 1
     get signup_path
     assert_select "title", full_title("Sign up")
+
+  end
+
+  test "users logged-in layout links" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", signup_path, count: 0
   end
 end
